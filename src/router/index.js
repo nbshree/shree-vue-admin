a3d5826c616ddc7ae778895dcc614d44e649786e
shree <shenruiyun@nbeport.com>
2019-12-12 15:01:16 +0800

    路由加入新参数single 左边链接表中是否为单页

--- src/router/index.js
+++ src/router/index.js
@@ -1,73 +1,75 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import _import from '@/utils/import';
+
 const Layout = _import('layout/index');
 
 Vue.use(VueRouter);
 
 export const constantRouterMap = [
-    {
-        path: '/redirect',
-        component: Layout,
-        hidden: true,
-        children: [
-            {
-                path: '/redirect/:path*',
-                component: _import('redirect/index')
-            }
-        ]
-    },
-    {
-        path: '/403',
-        component: _import('errorPage/403'),
-        hidden: true
-    },
-    {
-        path: '/404',
-        component: _import('errorPage/404'),
-        hidden: true
-    },
-    {
-        path: '/500',
-        component: _import('errorPage/500'),
-        hidden: true
-    },
-    {
-        path: '/login',
-        component: _import('login/index'),
-        hidden: true
-    },
-    {
-        path: '/lock',
-        component: _import('lock/index'),
-        name: 'lock',
-        hidden: true
-    },
-    {
-        path: '',
-        component: Layout,
-        redirect: 'dashboard',
-        children: [
-            {
-                path: 'dashboard',
-                name: 'dashboard',
-                component: _import('dashboard/index'),
-                meta: {
-                    title: '控制面板',
-                    icon: 'dashboard',
-                    permission: 'dashboard'
-                }
-            }
-        ]
-    }
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path*',
+        component: _import('redirect/index')
+      }
+    ]
+  },
+  {
+    path: '/403',
+    component: _import('errorPage/403'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: _import('errorPage/404'),
+    hidden: true
+  },
+  {
+    path: '/500',
+    component: _import('errorPage/500'),
+    hidden: true
+  },
+  {
+    path: '/login',
+    component: _import('login/index'),
+    hidden: true
+  },
+  {
+    path: '/lock',
+    component: _import('lock/index'),
+    name: 'lock',
+    hidden: true
+  },
+  {
+    path: '',
+    component: Layout,
+    redirect: 'dashboard',
+    single: true,
+    children: [
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: _import('dashboard/index'),
+        meta: {
+          title: '控制面板',
+          icon: 'dashboard',
+          permission: 'dashboard'
+        }
+      }
+    ]
+  }
 ];
 
 const createRouter = () =>
-    new VueRouter({
-        // mode: 'history', // require service support
-        // scrollBehavior: () => ({ y: 0 }),
-        routes: constantRouterMap
-    });
+  new VueRouter({
+    // mode: 'history', // require service support
+    // scrollBehavior: () => ({ y: 0 }),
+    routes: constantRouterMap
+  });
 
 const router = createRouter();
 //
