583636899186c18809d352d62afa57536c47ba91
shree <shenruiyun@nbeport.com>
2019-12-12 16:06:37 +0800

    table

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,7 @@
 import Mock from 'mockjs';
 import loginAPI from './login';
+import article from './article'
+import { param2Obj } from '@/utils/index'
 
 const openMock = true;
 
@@ -9,5 +11,27 @@ if (openMock) {
     Mock.mock(/\/user\/info/, 'get', loginAPI.info);
     Mock.mock(/\/user\/permission/, 'get', loginAPI.permission);
 }
-
+const mocks = [
+  ...article
+];
+for (const i of mocks) {
+  Mock.mock(new RegExp(i.url), i.type || 'get', XHR2ExpressReqWrap(i.response))
+}
+function XHR2ExpressReqWrap(respond) {
+  return function(options) {
+    let result = null
+    if (respond instanceof Function) {
+      const { body, type, url } = options
+      // https://expressjs.com/en/4x/api.html#req
+      result = respond({
+        method: type,
+        body: JSON.parse(body),
+        query: param2Obj(url)
+      })
+    } else {
+      result = respond
+    }
+    return Mock.mock(result)
+  }
+}
 export default Mock;
